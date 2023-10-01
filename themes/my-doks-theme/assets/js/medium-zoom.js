70fc5795c3ebfcc1d72dd9765681c586f8420505
LHU <haulrest@naver.com>
2023-10-02 02:12:29 +0900

    zoom img 추가 조정
    크기를 게시글 너비에 맞추지 않도록 변경
    스타일 조정
    이미지가 여러 개일 때 처리

--- themes/my-doks-theme/assets/js/medium-zoom.js
+++ themes/my-doks-theme/assets/js/medium-zoom.js
@@ -2,9 +2,9 @@ import mediumZoom from 'medium-zoom'
 
 
 document.addEventListener('lazybeforeunveil', () => {
-    mediumZoom('#zoom-default', {
+    const zoom = mediumZoom('#zoom-default', {
         margin: 70,
         background: 'rgba(00, 00, 00, .3)',
         scrollOffset: 0,
     })
-})
+}, { once: true })
