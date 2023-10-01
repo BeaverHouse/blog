f0f23140ade8321860e28b8cf95e6a05d20bd770
LHU <haulrest@naver.com>
2023-10-02 00:09:49 +0900

    이미지 클릭시 zoom 되도록 shortcode 작성

---
+++ themes/my-doks-theme/assets/js/medium-zoom.js
@@ -0,0 +1,10 @@
+import mediumZoom from 'medium-zoom'
+
+
+document.addEventListener('lazybeforeunveil', () => {
+    mediumZoom('#zoom-default', {
+        margin: 70,
+        background: 'rgba(00, 00, 00, .3)',
+        scrollOffset: 0,
+    })
+})
