7e1282eba48b51d255f9c9f032b49b18bf271a69
LHU <haulrest@naver.com>
2023-10-02 18:34:44 +0900

    medium-zoom style, tag link 조정

--- themes/my-doks-theme/assets/js/medium-zoom.js
+++ themes/my-doks-theme/assets/js/medium-zoom.js
@@ -3,7 +3,7 @@ import mediumZoom from 'medium-zoom'
 
 document.addEventListener('lazybeforeunveil', () => {
     const zoom = mediumZoom('#zoom-default', {
-        margin: 70,
+        margin: 10,
         background: 'rgba(00, 00, 00, .3)',
         scrollOffset: 0,
     })
