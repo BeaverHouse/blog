5346d0ad4eba6d74f784c5e9622811e6f2ce0f22
LHU <haulrest@gmail.com>
2023-11-15 21:37:35 +0900

    medium-zoom 수정
    overlay가 계속 남는 경우가 있어 강제삭제

--- themes/my-doks-theme/assets/js/medium-zoom.js
+++ themes/my-doks-theme/assets/js/medium-zoom.js
@@ -1,10 +1,24 @@
-import mediumZoom from 'medium-zoom'
+import mediumZoom from "medium-zoom";
 
-
-document.addEventListener('lazybeforeunveil', () => {
-    const zoom = mediumZoom('#zoom-default', {
+document.addEventListener(
+  "lazybeforeunveil",
+  () => {
+    const zoom = mediumZoom("#zoom-default", {
+      margin: 10,
+      background: "rgba(00, 00, 00, .3)",
+      scrollOffset: 0,
+    });
+    zoom.on("close", (event) => {
+      const elements = document.getElementsByClassName("medium-zoom-overlay");
+      while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+      }
+      mediumZoom("#zoom-default", {
         margin: 10,
-        background: 'rgba(00, 00, 00, .3)',
+        background: "rgba(00, 00, 00, .3)",
         scrollOffset: 0,
-    })
-}, { once: true })
+      });
+    });
+  },
+  { once: true }
+);
